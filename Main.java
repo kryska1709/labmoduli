e2f3884ee790e71b7157379cfc81b7cc5d61f55f
kryska1709 <157532841+kryska1709@users.noreply.github.com>
2024-01-24 12:22:12 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
+        System. out. println("введите  целое число n") ;
+        int n=scan.nextInt() ;
         System.out.println("введите х ,чтобы оно было больше 0.5");
         double x= scan.nextDouble();
         double sum=0;
         if (x > 0.5) {
-            for (int n = 0; n < 9; n++) {
+            for (int i=0; i< n; i++) {
                 double chislitel = Math.pow((x - 1), (n + 1));
                 double znamenatel = (n + 1) * (Math.pow((x + 1), (n + 1)));
                 double drobb = chislitel / znamenatel;
