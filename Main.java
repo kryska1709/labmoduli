bcb97075a6cbd1eda2e99bfc6528856beaf8986f
kryska1709 <157532841+kryska1709@users.noreply.github.com>
2024-02-07 11:31:34 +0300

    laba4

--- Main.java
+++ Main.java
@@ -1,22 +1,13 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
-        System.out.println("введите х ,чтобы оно было больше 0.5");
-        double x= scan.nextDouble();
-        double sum=0;
-        if (x > 0.5) {
-            for (int n = 0; n < 9; n++) {
-                double chislitel = Math.pow((x - 1), (n + 1));
-                double znamenatel = (n + 1) * (Math.pow((x + 1), (n + 1)));
-                double drobb = chislitel / znamenatel;
-                sum += drobb;
-            }
-            System.out.println(sum);
-        }
-        else
-        {
-            System.out.println("некорректное число,введите число удовлетворяющее условию");
+        Scanner scan = new Scanner(System.in);
+        int len = 12;
+        System.out.println("Введите целые положительные элементы массива");
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++) {
+            System.out.println(i);
+            array[i] = scan.nextInt();
         }
     }
 }
