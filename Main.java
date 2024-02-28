974675e40e46e9b804ca662245a42d8fbe81f0b7
kryska1709 <157532841+kryska1709@users.noreply.github.com>
2024-02-28 08:44:28 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,70 @@
-//Дан одномерный массив из 12 элементов. Отсортировать положительные элементы по возрастанию быстрой сортировкой
-import java.util.Arrays;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int len = 12;
-        System.out.println("Введите целые положительные элементы массива");
-        int[] sortArr = new int[len];
-        for (int i = 0; i < len; i++) {
-            System.out.println(i);
-            sortArr[i] = scan.nextInt();
+        Scanner ok = new Scanner(System.in);
+        System.out.println("введите число х");
+        double x = ok.nextDouble();
+        System.out.println("введите число a");
+        double a = ok.nextDouble();
+        System.out.println("введите число b");
+        double b = ok.nextDouble();
+            Calc calculate = new Calc();
+            System.out.println("y=3x+5 , будет равен: " + calculate.summa(x));
+            System.out.println("y=(a+b)/(a-b) , будет равен: " + calculate.del(a, b));
+            System.out.println("y=(ax/b)! , будет равен: " + calculate.fact(x, a, b));
+            Calc leo = new Calc();
+            System.out.println("введите число х");
+            x=ok.nextDouble();
+            System.out.println("введите число a");
+            a=ok.nextDouble();
+            System.out.println("введите число b");
+            b=ok.nextDouble();
+            System.out.println("y=3x+5 , будет равен: " + leo.summa(x));
+            System.out.println("y=(a+b)/(a-b) , будет равен: " + leo.del(a, b));
+            System.out.println("y=(ax/b)! , будет равен: " + leo.fact(x, a, b));
+            Calc rio = new Calc();
+            System.out.println("введите число х");
+            x=ok.nextDouble();
+            System.out.println("введите число a");
+            a=ok.nextDouble();
+            System.out.println("введите число b");
+            b=ok.nextDouble();
+            System.out.println("введите число b");
+            System.out.println("y=3x+5 , будет равен: " + rio.summa(x));
+            System.out.println("y=(a+b)/(a-b) , будет равен: " + rio.del(a, b));
+            System.out.println("y=(ax/b)! , будет равен: " + rio.fact(x, a, b));
         }
-        combSort(sortArr);
-        System.out.println(Arrays.toString(sortArr));
     }
-    public static void combSort(int[] sortArr) {
-       double factor=1.247;
-       int step=sortArr.length-1;
-       while(step>=1){
-           for(int i=0;i+step<sortArr.length;++i) {
-               if (sortArr[i] > sortArr[i + step]) {
-                   swap(sortArr, i, i + step);
-               }}
-           step /= factor;
-           }
+    class Calc {
+        public double summa(double x) {
+            double result = 3 * x + 5;
+            return result;
         }
-        public static void swap(int[] sortArr,int i,int j){
-        int swap=sortArr[i];
-        sortArr[i]=sortArr[j];
-        sortArr[j]=swap;
+
+        public double del(double a, double b) {
+            if (b == a) {
+                System.out.println("недопустимые значения, на ноль делить нельзя");
+                return 0;
+            } else {
+                double result = (a + b) / (a - b);
+                return result;
+
+            }
+        }
+
+        public double fact(double a, double x, double b) {
+            if (b == 0) {
+                System.out.println("недопустимые значения,  на ноль делить нельзя");
+                return 0;
+            } else {
+                double result = 1;
+                for (int i = 1; i <= (a * x) / b; i++) {
+                    result *= i;
+                }
+                return result;
+            }
         }
     }
+
+
+
