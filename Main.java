fe5211dc32ca3e4a07ebc0ca0ea875db9b2c2801
kryska1709 <157532841+kryska1709@users.noreply.github.com>
2024-02-28 12:07:21 +0300

    лаба6

--- Main.java
+++ Main.java
@@ -1,70 +1,67 @@
+//*Определить класс Reader, хранящий информацию о пользователе библиотеки: ФИО, номер читательского билета, факультет, дата рождения, телефон.
+//В программе создается массив из пяти объектов данного класса. Перегрузить методы takeBook(), returnBook():
+//- takeBook, который будет принимать количество взятых книг. Выводит на консоль сообщение "Петров В. В. взял 3 книги".
+//- takeBook, который будет принимать переменное количество названий книг. Выводит на консоль сообщение "Петров В. В. взял книги: Приключения, Словарь, Энциклопедия".
+//Аналогичным образом перегрузить метод returnBook(). Выводит на консоль сообщение "Петров В. В. вернул книги: Приключения, Словарь, Энциклопедия". Или "Петров В. В. вернул 3 книги".*//
+import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner ok = new Scanner(System.in);
-        System.out.println("введите число х");
-        double x = ok.nextDouble();
-        System.out.println("введите число a");
-        double a = ok.nextDouble();
-        System.out.println("введите число b");
-        double b = ok.nextDouble();
-            Calc calculate = new Calc();
-            System.out.println("y=3x+5 , будет равен: " + calculate.summa(x));
-            System.out.println("y=(a+b)/(a-b) , будет равен: " + calculate.del(a, b));
-            System.out.println("y=(ax/b)! , будет равен: " + calculate.fact(x, a, b));
-            Calc leo = new Calc();
-            System.out.println("введите число х");
-            x=ok.nextDouble();
-            System.out.println("введите число a");
-            a=ok.nextDouble();
-            System.out.println("введите число b");
-            b=ok.nextDouble();
-            System.out.println("y=3x+5 , будет равен: " + leo.summa(x));
-            System.out.println("y=(a+b)/(a-b) , будет равен: " + leo.del(a, b));
-            System.out.println("y=(ax/b)! , будет равен: " + leo.fact(x, a, b));
-            Calc rio = new Calc();
-            System.out.println("введите число х");
-            x=ok.nextDouble();
-            System.out.println("введите число a");
-            a=ok.nextDouble();
-            System.out.println("введите число b");
-            b=ok.nextDouble();
-            System.out.println("введите число b");
-            System.out.println("y=3x+5 , будет равен: " + rio.summa(x));
-            System.out.println("y=(a+b)/(a-b) , будет равен: " + rio.del(a, b));
-            System.out.println("y=(ax/b)! , будет равен: " + rio.fact(x, a, b));
+    Scanner ok = new Scanner(System.in);
+    int kolvoknig;
+    String [] nameKnig=new String[6];
+    nameKnig[0]="приключения тома сойера";
+    nameKnig[1]="все ради игры";
+    nameKnig[2]="буратино";
+    nameKnig[3]="сильнее страха";
+    nameKnig[4]="незнайка";
+    nameKnig[5]="изумрудный город";
+    Reader [] reads= new Reader[5];
+    reads[0]=new Reader("Иванов А.Б.",834792,"психология","17.09.2003","89263746215");
+    reads[1]=new Reader("Лисов И.В.",892637,"Физика","11.12.2004","89253236135");
+    reads[2]=new Reader("Кириллов А.И.",283716,"Экономика","13.01.2005","8926293215");
+    reads[3]=new Reader("Росик Д.Д.",928374,"Математика","23.04.2002","89263020915");
+    reads[4]=new Reader("Староверов Д.Л.",162734,"Медицинский","29.02.2004","8920900215");
+    reads[0].takeBook(6);
+    reads[1].takeBook(nameKnig[1], nameKnig[2], nameKnig[3]);
+    reads[2].returnBook(5);
+    reads[3].returnBook(nameKnig[0], nameKnig[1]);
+    reads[4].takeBook(1);
+    }
+}
+class Reader{
+    private String Name;
+    private int Nomerbil;
+    private String Facultet;
+    private String Daterozh;
+    private String Phone;
+    public Reader(String Name, int Nomerbil,String Facultet,String Daterozh,String Phone){
+        this.Name=Name;
+        this.Nomerbil=Nomerbil;
+        this.Facultet=Facultet;
+        this.Daterozh=Daterozh;
+        this.Phone=Phone;
+    }
+    public void takeBook(int kolvoknig){
+        System.out.println(this.Name+"взял "+kolvoknig+" книг");
+    }
+    public void takeBook(String... nameKnig){
+        System.out.println(this.Name+"взял ");
+        for (String knigi :nameKnig ) {
+            System.out.print("\"" + knigi + "\""  + ", ");
+        }
+        System.out.println();
+    }
+    public void returnBook(int kolvoknig){
+        System.out.println(this.Name+"вернул "+kolvoknig+" книг");
+    }
+    public void returnBook(String... nameKnig){
+        System.out.println(this.Name+"вернул ");
+            for (String knigi : nameKnig) {
+                System.out.print("\"" + knigi + "\""  + ", ");
+            }
+        System.out.println();
         }
     }
-    class Calc {
-        public double summa(double x) {
-            double result = 3 * x + 5;
-            return result;
-        }
-
-        public double del(double a, double b) {
-            if (b == a) {
-                System.out.println("недопустимые значения, на ноль делить нельзя");
-                return 0;
-            } else {
-                double result = (a + b) / (a - b);
-                return result;
-
-            }
-        }
-
-        public double fact(double a, double x, double b) {
-            if (b == 0) {
-                System.out.println("недопустимые значения,  на ноль делить нельзя");
-                return 0;
-            } else {
-                double result = 1;
-                for (int i = 1; i <= (a * x) / b; i++) {
-                    result *= i;
-                }
-                return result;
-            }
-        }
-    }
-
-
-
+//"\"" + nameKnig[0] + "\""  + ", " + "\"" + nameKnig[3] + "\""
+//"\"" + nameKnig[1] + "\""  + ", " + "\"" + nameKnig[2] +  "\"" + ", " + "\""+ nameKnig[0] + "\"" + ", " + "\""+ nameKnig[4]+"\"" + ", " + "\""+nameKnig[5]+"\""
